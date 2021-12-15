fcc6e789aac6200c4f66b39a36cb9f485ff8e7d7
DylanToporek <dylantoporek@gmail.com>
2021-12-15 12:59:40 -0500

    commit tree 88795b1bde2ab073fd465b4a113638fd0acd721c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,11 +1,11 @@
 class CashRegister
-    attr_accessor :total, :discount, :items
+    attr_accessor :total, :discount, :items, :last_transaction
     
-    def initialize(discount = 0)``
+    def initialize(discount = 0)
         @total = 0
         @discount = discount
         @items = []
-
+        @last_transaction = 0
     end
 
     def total
@@ -13,14 +13,15 @@ class CashRegister
     end
 
     def add_item(title, price, quantity = 1)
-       @total = (price.to_f * quantity) + @total
-       @itmes << title
+        @last_transaction = price.to_f * quantity
+       @total += @last_transaction
+       quantity.times do |i|
+        @items << title
+       end
     end
 
     def apply_discount
         if @discount != 0
-            puts @total
-            print @total - (@total * @discount/100)
             @total = (@total - (@total * @discount/100)).to_i
             "After the discount, the total comes to $#{@total}."
         elsif @discount == 0
@@ -28,7 +29,7 @@ class CashRegister
         end
     end
 
-    def items
-        @itmes
+    def void_last_transaction
+        @total = @total - @last_transaction
     end
 end
